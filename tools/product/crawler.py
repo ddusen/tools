df012117bbc8f4ed66c62102f19bd9da4fc36502
Sdu <sdu@shendu.info>
2017-04-26 08:54:21 +0800

    improve product/crawler.py

--- tools/product/crawler.py
+++ tools/product/crawler.py
@@ -34,7 +34,7 @@ CREATE TABLE `product` (
 """
 
 header = {
-    'Cookie': 'AD_RS_COOKIE=20083361; _gscu_1771678062=93019765gxzmln75; _gscs_1771678062=93019765adenxt75|pv:4; _gscbrs_1771678062=1; _trs_uv=23e1_6_j1vtf47y; _trs_ua_s_1=csq7_6_j1vtf47y',
+    'Cookie': '_gscu_1771678062=93019765gxzmln75; _gscbrs_1771678062=1; _trs_uv=23e1_6_j1vtf47y; AD_RS_COOKIE=20083361',
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36',
 }
 
@@ -75,33 +75,49 @@ def level_one():
 
 
 def level_two():
-    request_url = 'http://www.stats.gov.cn/tjsj/tjbz/tjypflml/2010/%s.html'
     level_one_tuple = query(
         sql=u"SELECT `id`, `code` FROM product WHERE level = 1")
     for i in level_one_tuple:
-        code = i.get('code')
+        code = str(i.get('code'))
         sql_id = i.get('id')
-        html_data = extract_item_by_re(get_response(url=request_url % code, encoding='gb2312', headers=header).text,
-                                       r"<tr class='citytr'><td><a href='../....\.html'>(.*?)</a></td><td><a href='../....\.html'>(.*?)</a></td></tr>")
-        handle_data_two(data=html_data, level=2, parent_id=sql_id)
+
+        request_url = 'http://www.stats.gov.cn/tjsj/tjbz/tjypflml/2010/%s.html' % code
+        print request_url
+
+        def excute():
+            try:
+                html_data = extract_item_by_re(get_response(url=request_url, encoding='gb2312', headers=header).text,
+                                               r"<tr class='citytr'><td><a href='../....\.html'>(.*?)</a></td><td><a href='../....\.html'>(.*?)</a></td></tr>")
+            except Exception as e:
+                print e
+                time.sleep(3)
+                excute()
+
+            return html_data
+
+        handle_data_two(data=excute(), level=2, parent_id=sql_id)
         time.sleep(1)
 
     return True
 
 
 def level_three():
-    request_url_three = 'http://www.stats.gov.cn/tjsj/tjbz/tjypflml/2010/%s/%s.html'
     level_one_tuple = query(
         sql=u"SELECT `id`, `code` FROM product WHERE level = 1")
     for i in level_one_tuple:
-        code_one = i.get('code')
+        code_one = str(i.get('code'))
         id_one = i.get('id')
         level_two_tuple = query(
             sql=u"SELECT `id`, `code` FROM product WHERE level = 2 AND parent_id = %s ", list1=(id_one, ))
         for j in level_two_tuple:
-            code_two = j.get('code')
+            code_two = str(j.get('code'))
             id_two = j.get('id')
-            html_data = extract_item_by_re(get_response(url=request_url_three % (code_one, code_two), encoding='gb2312', headers=header).text,
+
+            request_url = 'http://www.stats.gov.cn/tjsj/tjbz/tjypflml/2010/%s/%s.html' % (
+                code_one, code_two)
+            print request_url
+
+            html_data = extract_item_by_re(get_response(url=request_url, encoding='gb2312', headers=header).text,
                                            r"<tr class='countytr'><td><a href='../(......)\.html'>.*?</a></td><td><a href='../......\.html'>(.*?)</a></td></tr>")
             handle_data_two(data=html_data, level=3, parent_id=id_two)
             time.sleep(1)
@@ -110,32 +126,35 @@ def level_three():
 
 
 def level_four():
-    request_url_four = 'http://www.stats.gov.cn/tjsj/tjbz/tjypflml/2010/%s/%s/%s.html'
     level_one_tuple = query(
         sql=u"SELECT `id`, `code` FROM product WHERE level = 1")
     for i in level_one_tuple:
-        code_one = i.get('code')
+        code_one = str(i.get('code'))
         id_one = i.get('id')
 
         level_two_tuple = query(
             sql=u"SELECT `id`, `code` FROM product WHERE level = 2 AND parent_id = %s ", list1=(id_one, ))
         for j in level_two_tuple:
-            code_two = j.get('code')
+            code_two = str(j.get('code'))
             id_two = j.get('id')
 
             level_three_tuple = query(
                 sql=u"SELECT `id`, `code` FROM product WHERE level = 3 AND parent_id = %s ", list1=(id_two, ))
             for k in level_three_tuple:
-                code_three = k.get('code')
+                code_three = str(k.get('code'))
                 id_three = k.get('id')
+
+                request_url = 'http://www.stats.gov.cn/tjsj/tjbz/tjypflml/2010/%s/%s/%s.html' % (
+                    code_one, str(code_two)[2:4:1], code_three)
+                print request_url
 
                 def excute():
                     try:
-                        html_data = extract_item_by_re(get_response(url=request_url_four % (code_one, str(code_two)[2:4:1], code_three), encoding='gb2312', headers=header).text,
-                                                   r"<tr class='towntr'><td><a href='../(........)\.html'>.*?</a></td><td><a href='../........\.html'>(.*?)</a></td></tr>")
+                        html_data = extract_item_by_re(get_response(url=request_url, encoding='gb2312', headers=header).text,
+                                                       r"<tr class='towntr'><td><a href='../(........)\.html'>.*?</a></td><td><a href='../........\.html'>(.*?)</a></td></tr>")
                         if html_data == []:
-                            html_data = extract_item_by_re(get_response(url=request_url_four % (
-                                code_one, str(code_two)[2:4:1], code_three), encoding='gb2312', headers=header).text, r"<tr class='villagetr'><td>(.*?)</td><td>(.*?)</td>")
+                            html_data = extract_item_by_re(get_response(
+                                url=request_url, encoding='gb2312', headers=header).text, r"<tr class='villagetr'><td>(.*?)</td><td>(.*?)</td>")
                     except Exception as e:
                         print e
                         time.sleep(3)
@@ -150,54 +169,60 @@ def level_four():
 
 
 def level_five():
-    request_url_five = 'http://www.stats.gov.cn/tjsj/tjbz/tjypflml/2010/%s/%s/%s/%s.html'
     level_one_tuple = query(
         sql=u"SELECT `id`, `code` FROM product WHERE level = 1")
     for i in level_one_tuple:
-        code_one = i.get('code')
+        code_one = str(i.get('code'))
         id_one = i.get('id')
 
         level_two_tuple = query(
             sql=u"SELECT `id`, `code` FROM product WHERE level = 2 AND parent_id = %s ", list1=(id_one, ))
         for j in level_two_tuple:
-            code_two = j.get('code')
+            code_two = str(j.get('code'))
             id_two = j.get('id')
 
             level_three_tuple = query(
                 sql=u"SELECT `id`, `code` FROM product WHERE level = 3 AND parent_id = %s ", list1=(id_two, ))
             for k in level_three_tuple:
-                code_three = k.get('code')
+                code_three = str(k.get('code'))
                 id_three = k.get('id')
 
                 level_four_tuple = query(
                     sql=u"SELECT `id`, `code` FROM product WHERE level = 4 AND parent_id = %s ", list1=(id_three, ))
                 for x in level_four_tuple:
-                    code_four = x.get('code')
+                    code_four = str(x.get('code'))
                     id_four = x.get('id')
+
+                    request_url = 'http://www.stats.gov.cn/tjsj/tjbz/tjypflml/2010/%s/%s/%s/%s.html' % (code_one,  str(
+                        code_two)[2:4:1],  str(code_three)[4:6:1], code_four)
+                    print request_url
 
                     def excute():
                         try:
-                            html_data = extract_item_by_re(get_response(url=request_url_five % (code_one,  str(code_two)[2:4:1],  str(code_three)[4:6:1], code_four), encoding='gb2312', headers=header).text, r"<tr class='villagetr'><td>(.*?)</td><td>(.*?)</td>")
+                            html_data = extract_item_by_re(get_response(
+                                url=request_url, encoding='gb2312', headers=header).text, r"<tr class='villagetr'><td>(.*?)</td><td>(.*?)</td>")
                         except Exception as e:
                             print e
                             time.sleep(3)
+                            html_data = extract_item_by_re(get_response(
+                                url=request_url, encoding='gb2312', headers=header).text, r"<tr class='villagetr'><td>(.*?)</td><td>(.*?)</td>")
                             excute()
 
                         return html_data
-                        
-                        handle_data_two(data=excute(), level=5, parent_id=id_four)
+
+                    handle_data_two(data=excute(), level=5, parent_id=id_four)
                     time.sleep(1)
 
     return True
 
 
 def main():
-    # if level_one():
-    #     if level_two():
-            # if level_three():
-    if level_four():
-        if level_five():
-            print "EXCUTE SUCCESS!"
+    if level_one():
+        if level_two():
+            if level_three():
+                if level_four():
+                    if level_five():
+                        print "EXCUTE SUCCESS!"
 
 
 if __name__ == '__main__':
