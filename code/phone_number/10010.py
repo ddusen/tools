81a816e0e14cf381f0d4b8fdb669a237b79c64a5
Sd <sdu@shendu.info>
2017-09-24 19:53:12 +0800

    improve phone_number

--- code/phone_number/10010.py
+++ code/phone_number/10010.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+'''
+解决茫茫号码群挑选的繁琐，
+帮你轻松选出心仪的号码，
+所想即所得。
+'''
 import sys
 import time
 sys.path.append('/home/sdu/Project/tools/code/utils/crawler')
@@ -7,7 +12,8 @@ sys.path.append('/home/sdu/Project/tools/code/utils/crawler')
 from process import get_response
 from mysql import query, query_one, save
 
-def main():
+
+def save_phone():
 	header = {
 		'Cookie':'gipgeo=71|710; _n3fa_cid=00f3d758a5314bcdf0563cf8a25851b6; _n3fa_ext=ft=1506245499; _n3fa_lvt_a9e72dfe4a54a20c3d6e671b3bad01d9=1506245499; _n3fa_lpvt_a9e72dfe4a54a20c3d6e671b3bad01d9=1506245499; TC_CD=94921d44-95e1-486e-8e5c-4cd1e3b4c81c; mallcity=51%7C540',
 		'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36'
@@ -27,6 +33,12 @@ def main():
 					print d
 
 
+
+def main():
+	# 186,185 开头, 不含4
+	pn_list = query(sql=u'SELECT * FROM `phone_number` WHERE (`number` LIKE %s OR `number` LIKE %s) AND `number` NOT LIKE %s', list1=('186%', '185%', '%4%', ))
+	for p in pn_list:
+		print p
 
 if __name__ == '__main__':
 	main()
