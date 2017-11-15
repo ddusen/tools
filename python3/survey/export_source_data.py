72223acf80ba02264ad504162d8f304be71b47a3
Sd <sdu@shendu.info>
2017-11-15 17:58:13 +0800

    bug fix

--- python3/survey/export_source_data.py
+++ python3/survey/export_source_data.py
@@ -8,14 +8,15 @@ from utils.office.excel import write_xlsx
 
 
 def get_data():
-    return requests.get('http://localhost:8001/api/data').json()
+    return requests.get('http://localhost:8000/api/data').json()
 
 
 def main():
     # Iterate over the data and write it out row by row.
+    # get_index = lambda x: 0 if k == 'start_at' else 1 if k == 'end_at' else 2 if k == 'phone' else 3 if k == 'city' else 4 if k == 'email' else int(k)+4
     # for r_index, r_data in enumerate(data):
-    #     for c_index, c_data in enumerate(sorted(r_data.items(), reverse=True)):
-    #         worksheet.write(r_index, c_index, c_data[1])
+    #     for k in r_data.keys():
+    #         worksheet.write(r_index, get_index(k), r_data.get(k))
     write_xlsx('/home/sdu/Documents/tools/python3/survey/survey_data.xlsx', get_data().get('data'));
 
 
